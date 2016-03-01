163ec9353e8df376948a6d6d75da7053bfcab056
Cem Basoglu <cem.basoglu@web.de>
2016-03-01 01:27:11 +0100

    VisibiltyConverter added

---
+++ GroundTools/xeniC.AnySense.KonfigurationManager/xeniC.AnySense.Controls/Converter/VisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace xeniC.AnySense.Controls.Converter
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public VisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Hidden;
+        }
+
+        public Visibility TrueValue { get; set; }
+        public Visibility FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+                return ((bool)value) ? TrueValue : FalseValue;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
