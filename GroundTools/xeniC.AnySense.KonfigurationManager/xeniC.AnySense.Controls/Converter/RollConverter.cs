41c7310587874c877c58b9604afc58462cb5fcef
Cem Basoglu <cem.basoglu@web.de>
2016-03-04 20:06:17 +0100

    Roll Converter to invert roll view

---
+++ GroundTools/xeniC.AnySense.KonfigurationManager/xeniC.AnySense.Controls/Converter/RollConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace xeniC.AnySense.Controls.Converter
+{
+    class RollConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                value = -(double)value;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
