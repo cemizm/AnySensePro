0a5b06b9050699553e790a70fe6bedf5ecc1bd91
Cem Basoglu <cem.basoglu@web.de>
2016-03-12 18:05:40 +0100

    AnySense Studio final style

---
+++ GroundTools/xeniC.AnySense.KonfigurationManager/xeniC.AnySense.Controls/Compass.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xeniC.AnySense.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für Compass.xaml
+    /// </summary>
+    public partial class Compass : UserControl
+    {
+        // Using a DependencyProperty as the backing store for Heading.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeadingProperty =
+            DependencyProperty.Register("Heading", typeof(double), typeof(Compass), new PropertyMetadata(0d));
+
+
+        public Compass()
+        {
+            InitializeComponent();
+        }
+
+        public double Heading
+        {
+            get { return (double)GetValue(HeadingProperty); }
+            set { SetValue(HeadingProperty, value); }
+        }
+
+    }
+}
---
+++ GroundTools/xeniC.AnySense.KonfigurationManager/xeniC.AnySense.Controls/Converter/AltitudeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace xeniC.AnySense.Controls.Converter
+{
+    class AltitudeConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double ret;
+            if (value != null && double.TryParse(value.ToString(), out ret))
+                return ((int)(ret * 10)) % 100 * 4;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
