957a9363736173367bf55bc02f1ec5eddf28c760
Cem Basoglu <cem.basoglu@web.de>
2016-03-22 02:35:19 +0100

    Jeti Telemetry finished

--- Firmware/shared/Application/include/TelemetryJeti.h
+++ Firmware/shared/Application/include/TelemetryJeti.h
@@ -10,6 +10,8 @@
 
 #include "TelemetryAdapter.h"
 
+#include "stdlib.h"
+
 #include "Timer.h"
 #include "USART.h"
 #include "Pin.h"
@@ -24,21 +26,232 @@ namespace App
 class TelemetryJeti: public HAL::InterruptHandler, public TelemetryAdapter<TelemetryProtocol::Jeti>
 {
 private:
-	static const uint16_t prescaler = 8;
-	static const uint16_t baudrate = 9800;
+	static const uint16_t Prescaler = 8;
+	static const uint16_t Baudrate = 9800;
+
+	static const uint8_t DataInterval = 19;
+	static const uint8_t TextInterval = 2;
+
+	static const uint8_t StartByte = 0x7E;
+	static const uint8_t CRCPoly = 0x07;
+
+	static const uint8_t TelemetryValues = 19;
+	static const uint8_t MaxTelemetryCount = 15;
+	static const uint8_t MaxLabelCount = 16;
+
+	static const uint8_t MaxPacketSize = 29;
+	static const uint8_t MaxHeaderSize = 8;
+	static const uint8_t MaxCRCSize = 1;
+
+	static const uint8_t MaxSimpleTextSize = 34;
+
+	static const uint8_t MaxBufferSize = MaxPacketSize + MaxSimpleTextSize;
+
+	static const uint8_t MaxLabelName = 10;
+	static const uint8_t MaxLabelUnit = 3;
+
+	static const uint8_t MaxContentSize = MaxPacketSize - MaxHeaderSize - MaxCRCSize;
+
+	struct LabelType
+	{
+		uint8_t Name[MaxLabelName + 1];
+		uint8_t Unit[MaxLabelUnit + 1];
+	};
+
+	static const LabelType Labels[TelemetryValues + 1];
+
+	enum TelemetryValue
+	{
+		None = 0,
+		Latitude = 1,
+		Longitude = 2,
+		Satellite = 3,
+		GPS_Fix = 4,
+		Flightmode = 5,
+		Speed = 6,
+		Distance = 7,
+		Altitude = 8,
+		VSpeed = 9,
+		Compass = 10,
+		COG = 11,
+		HomeDirection = 12,
+		Voltage = 13,
+		Current = 14,
+		Capacity = 15,
+		LipoVoltage = 16,
+		LipoTemp = 17,
+		HomeLatitude = 18,
+		HomeLongitude = 19,
+
+	};
 
 	struct ProtocolConfig
 	{
 		uint16_t IsValid;
+		TelemetryValue Mapping[MaxTelemetryCount];
 	};
+
+	struct Type6
+	{
+		uint8_t Data :5;
+		uint8_t Decimalpoint :2;
+		uint8_t Sign :1;
+	}__attribute__((packed, aligned(1)));
+
+	struct Type14
+	{
+		uint16_t Data :13;
+		uint8_t Decimalpoint :2;
+		uint8_t Sign :1;
+	}__attribute__((packed, aligned(1)));
+
+	struct Type22
+	{
+		uint32_t Data :21;
+		uint8_t Decimalpoint :2;
+		uint8_t Sign :1;
+	}__attribute__((packed, aligned(1)));
+
+	struct Type30
+	{
+		uint32_t Data :29;
+		uint8_t Decimalpoint :2;
+		uint8_t Sign :1;
+	}__attribute__((packed, aligned(1)));
+
+	struct TypeGPS
+	{
+		uint16_t Minutes;
+		uint16_t Degree :13;
+		uint8_t IsLongitutde :1; //lat=0; lon=1;
+		uint8_t IsSouthWest :1; //North/East = 0; South/West = 1
+		uint8_t Sign :1; //unused??
+	}__attribute__((packed, aligned(1)));
+
+	struct DataPacket
+	{
+		uint8_t DataType :4;
+		uint8_t Id :4;
+		union
+		{
+			uint8_t Data[4];
+			Type6 U6;
+			Type14 U14;
+			Type22 U22;
+			Type30 U30;
+			TypeGPS GPS;
+		};
+
+		uint8_t UpdateU6(uint8_t decimal, int8_t val)
+		{
+			DataType = 0;
+			U6.Sign = val < 0;
+			U6.Decimalpoint = decimal;
+			U6.Data = abs(val);
+
+			return 2;
+		}
+		uint8_t UpdateU14(uint8_t decimal, int16_t val)
+		{
+			DataType = 1;
+			U14.Sign = val < 0;
+			U14.Decimalpoint = decimal;
+			U14.Data = abs(val);
+
+			return 3;
+		}
+		uint8_t UpdateU22(uint8_t decimal, int32_t val)
+		{
+			DataType = 4;
+			U22.Sign = val < 0;
+			U22.Decimalpoint = decimal;
+			U22.Data = abs(val);
+
+			return 4;
+		}
+		uint8_t UpdateU30(uint8_t decimal, int32_t val)
+		{
+			DataType = 8;
+			U30.Sign = val < 0;
+			U30.Decimalpoint = decimal;
+			U30.Data = abs(val);
+
+			return 5;
+		}
+
+		uint8_t UpdateGPS(double degree, uint8_t isLon)
+		{
+
+			DataType = 9;
+
+			GPS.IsLongitutde = isLon;
+			GPS.IsSouthWest = degree < 0 ? 1 : 0;
+
+			if (degree < 0)
+				degree = -degree;
+
+			uint16_t tmp = degree;
+
+			GPS.Degree = tmp;
+			tmp = (degree - tmp) * 60000;
+			GPS.Minutes = tmp;
+
+			return 5;
+		}
+
+	}__attribute__((packed, aligned(1)));
+
+	struct TextPacket
+	{
+		uint8_t Id;
+		uint8_t UnitLength :3;
+		uint8_t DescriptionLength :5;
+		uint8_t Label[MaxContentSize - 2];
+	}__attribute__((packed, aligned(1)));
+
+	struct
+	{
+		uint16_t SendBuffer[MaxBufferSize];
+		uint16_t CurrentData;
+		uint8_t BufferLength;
+		uint8_t BufferPos;
+		union
+		{
+			uint8_t Data[MaxPacketSize];
+			struct
+			{
+				uint8_t Seperator; 			//FIX: 0x7E
+				uint8_t ExId; 			//FIX: 0x9F Distinct identification of an EX packet, N could be an arbitrary number.
+				uint8_t Length :6; 			//Length of a packet (number of bytes following)
+				uint8_t Type :2; 			//Packet type; 1 – Data protocol, 0 - Text protocol
+				uint16_t ManufactureId; 			//Upper part of a serial number, Manufacturer ID (Little Endian)
+				uint16_t DeviceId; 			//Lower part of a serial number, Device ID (Little Endian)
+				uint8_t Reserved; 			//FIX: 0x00 reserved
+				uint8_t Content[MaxContentSize];
+			}__attribute__((packed, aligned(1)));
+		};
+	} m_tx;
 
 	HAL::USART& m_usart;
 	const HAL::Pin& m_pin;
 	HAL::Timer& m_timer;
 	ProtocolConfig* m_config;
 	uint8_t m_run;
-	uint8_t m_counter;
+
+	uint8_t m_bitPos;
+	uint8_t m_parity;
+
+	uint_fast32_t m_startTick;
+	uint_fast32_t m_nextText;
+	uint_fast32_t m_tick;
+
+	uint8_t m_currentLabel;
+
+	uint8_t m_currentValue;
+	uint8_t m_tmpValue;
+
 	void Reset();
+	void UpdateBuffer();
 protected:
 	void Init() override;
 	void Run(void) override;
@@ -46,10 +259,12 @@ protected:
 	void DeInit() override;
 public:
 	TelemetryJeti(HAL::USART& usart, HAL::Timer& timer) :
-			m_usart(usart), m_pin(usart.GetTXPin()), m_timer(timer), m_config(nullptr), m_run(1), m_counter(0)
+			m_usart(usart), m_pin(usart.GetTXPin()), m_timer(timer), m_config(nullptr), m_run(1), m_bitPos(0), m_parity(0), m_startTick(
+					0), m_nextText(0), m_tick(0), m_currentLabel(0), m_currentValue(0), m_tmpValue(0)
 	{
 	}
-};
+}
+;
 
 } /* namespace App */
 
--- Firmware/shared/Application/source/TelemetryJeti.cpp
+++ Firmware/shared/Application/source/TelemetryJeti.cpp
@@ -6,13 +6,21 @@
  */
 
 #include <TelemetryJeti.h>
+#include "SensorStore.h"
 
 #include "Pin.h"
+
+#include "string.h"
 
 namespace App
 {
 
 static_assert (sizeof(TelemetryJeti) <= TELEMETRY_WORKSPACE, "TelemetryJeti will not fit!");
+
+const TelemetryJeti::LabelType TelemetryJeti::Labels[TelemetryJeti::TelemetryValues + 1] = { { "AnySense", "" }, { "Lat.", "" }, {
+		"Lon.", "" }, { "Num Sat.", "" }, { "GPS Fix", "" }, { "F. Mode", "" }, { "Speed", "kmh" }, { "Dist.", "m" }, { "Alt.",
+		"m" }, { "Climb", "m/s" }, { "Comp.", "\xB0" }, { "F. Dir", "\xB0" }, { "H. Dir", "\xB0" }, { "Bat.", "V" }, { "Curr.",
+		"A" }, { "Capa.", "Ah" }, { "Cell", "V" }, { "Temp.", "\xB0" "C" }, { "H. Lat", "" }, { "H. Long", "" } };
 
 void TelemetryJeti::Init()
 {
@@ -22,38 +30,193 @@ void TelemetryJeti::Init()
 	{
 		m_config->IsValid = ConfigKey;
 
+		m_config->Mapping[0] = TelemetryValue::Latitude;
+		m_config->Mapping[1] = TelemetryValue::Longitude;
+		m_config->Mapping[2] = TelemetryValue::Satellite;
+		m_config->Mapping[3] = TelemetryValue::GPS_Fix;
+		m_config->Mapping[4] = TelemetryValue::Speed;
+		m_config->Mapping[5] = TelemetryValue::Distance;
+		m_config->Mapping[6] = TelemetryValue::Altitude;
+		m_config->Mapping[7] = TelemetryValue::VSpeed;
+		m_config->Mapping[8] = TelemetryValue::Compass;
+		m_config->Mapping[9] = TelemetryValue::HomeDirection;
+		m_config->Mapping[10] = TelemetryValue::Voltage;
+		m_config->Mapping[11] = TelemetryValue::Current;
+		m_config->Mapping[12] = TelemetryValue::Capacity;
+		m_config->Mapping[13] = TelemetryValue::LipoVoltage;
+		m_config->Mapping[14] = TelemetryValue::LipoTemp;
+
 		Config.Save();
 	}
 
 	m_pin.PowerUp();
-	m_pin.On();
 	m_pin.ModeSetup(GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP);
 	m_pin.SetOutputOptions(GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ);
+	m_pin.On();
 
 	m_timer.PowerUp();
 	m_timer.SetClockDivision(TIM_CR1_CKD_CK_INT);
 	m_timer.DirectionUp();
-	m_timer.SetPrescaler(prescaler - 1);
-	m_timer.SetPeriod((m_timer.clock_frequency / prescaler / baudrate) - 1);
+	m_timer.SetPrescaler(Prescaler - 1);
+	m_timer.SetPeriod((m_timer.clock_frequency / Prescaler / Baudrate) - 1);
 
 	m_timer.ClearFlag(TIM_SR_UIF);
 	m_timer.EnableIRQ(TIM_DIER_UIE);
 
 	HAL::InterruptRegistry.Enable(m_timer.NVIC_IRQn, PriorityTelemetry, this);
 }
+
 void TelemetryJeti::Run(void)
 {
-	while (m_run)
+
+	m_startTick = OSAL::Timer::GetTime() + delay_sec(10);
+	m_nextText = OSAL::Timer::GetTime();
+
+	m_tx.Seperator = StartByte;
+	m_tx.ExId = 0x9F;
+	m_tx.ManufactureId = 0xA4CB;
+	m_tx.DeviceId = 0x24;
+
+	DataPacket* packet = nullptr;
+	uint8_t packetLen = 0;
+	uint8_t totalLen = 0;
+	do
 	{
-		eventFlag.wait(delay_ms(100));
-		if (m_run)
+		eventFlag.wait(delay_ms(DataInterval));
+
+		m_tick = OSAL::Timer::GetTime();
+
+		m_tx.Length = 6;
+		memset(m_tx.Content, 0x00, MaxContentSize);
+
+		if (m_tick > m_nextText || m_startTick > m_tick)
 		{
-			m_counter = 0;
-			m_timer.Enable();
+			// EX Text
+			m_tx.Type = 0;
+
+			const LabelType text = Labels[m_currentLabel == 0 ? 0 : m_config->Mapping[m_currentLabel - 1]];
+
+			TextPacket* label = (TextPacket*) m_tx.Content;
+			label->Id = m_currentLabel;
+			label->DescriptionLength = strlen((const char*) text.Name);
+			label->UnitLength = strlen((const char*) text.Unit);
+
+			memcpy(label->Label, text.Name, label->DescriptionLength);
+			memcpy(&label->Label[label->DescriptionLength], text.Unit, label->UnitLength);
+
+			m_tx.Length += label->DescriptionLength + label->UnitLength + 2;
+			m_currentLabel = (m_currentLabel + 1) % MaxLabelCount;
+
+			m_nextText = m_tick + delay_sec(TextInterval);
 		}
-	}
+		else
+		{
+			//EX Data
+			m_tx.Type = 1;
+
+			m_tmpValue = m_currentValue;
+			totalLen = 0;
+
+			do
+			{
+				packet = (DataPacket*) &m_tx.Content[totalLen];
+				packetLen = 0;
+
+				switch (m_config->Mapping[m_currentValue])
+				{
+				case TelemetryValue::Latitude: //Lat
+					if (SensorData.IsGPSOK())
+						packetLen = packet->UpdateGPS(SensorData.GetPositionCurrent().Latitude, 0);
+					break;
+				case TelemetryValue::Longitude: //lon
+					if (SensorData.IsGPSOK())
+						packetLen = packet->UpdateGPS(SensorData.GetPositionCurrent().Longitude, 1);
+					break;
+				case TelemetryValue::Satellite: //sat
+					packetLen = packet->UpdateU6(0, SensorData.GetSatellites());
+					break;
+				case TelemetryValue::GPS_Fix: //gps fix
+					packetLen = packet->UpdateU6(0, SensorData.GetFixType());
+					break;
+				case TelemetryValue::Flightmode: //mode
+					packetLen = packet->UpdateU6(0, SensorData.GetFlightMode());
+					break;
+				case TelemetryValue::Speed: //speed
+					if (SensorData.IsGPSOK())
+						packetLen = packet->UpdateU14(1, SensorData.GetSpeed() * 36);
+					break;
+				case TelemetryValue::Distance: //distance
+					if (SensorData.IsGPSOK())
+						packetLen = packet->UpdateU22(2, SensorData.GetHomeDistance() * 100);
+					break;
+				case TelemetryValue::Altitude: //alt
+					packetLen = packet->UpdateU22(2, SensorData.GetRelativeAltitude() * 100);
+					break;
+				case TelemetryValue::VSpeed: //vsi
+					packetLen = packet->UpdateU22(2, SensorData.GetVerticalSpeed() * 100);
+					break;
+				case TelemetryValue::Compass: //heading
+					packetLen = packet->UpdateU14(1, SensorData.GetHeading() * 10);
+					break;
+				case TelemetryValue::COG: //cog
+					if (SensorData.IsGPSOK())
+						packetLen = packet->UpdateU14(1, SensorData.GetCourseOverGround() * 10);
+					break;
+				case TelemetryValue::HomeDirection: //home dir
+					if (SensorData.IsPositionHomeSet())
+						packetLen = packet->UpdateU14(1, SensorData.GetHomeDirection() * 10);
+					break;
+				case TelemetryValue::Voltage: //voltage
+					packetLen = packet->UpdateU14(2, SensorData.GetBattery() / 10.0f);
+					break;
+				case TelemetryValue::Current: //current
+					packetLen = packet->UpdateU14(1, SensorData.GetCurrent() * 10);
+					break;
+				case TelemetryValue::Capacity: //capa
+					packetLen = packet->UpdateU14(1, SensorData.GetCapacity() / 10.0f);
+					break;
+				case TelemetryValue::LipoVoltage: //cell
+					packetLen = packet->UpdateU14(2, SensorData.GetCellLowest() / 10.0f);
+					break;
+				case TelemetryValue::LipoTemp: //cell temp.
+					packetLen = packet->UpdateU14(1, SensorData.GetTemperatur1() * 10);
+					break;
+				case TelemetryValue::HomeLatitude: //Lat
+					if (SensorData.IsPositionHomeSet())
+						packetLen = packet->UpdateGPS(SensorData.GetPositionHome().Latitude, 0);
+					break;
+				case TelemetryValue::HomeLongitude: //lon
+					if (SensorData.IsPositionHomeSet())
+						packetLen = packet->UpdateGPS(SensorData.GetPositionHome().Longitude, 1);
+					break;
+				default:
+					packetLen = 0;
+					break;
+				}
+
+				if ((totalLen + packetLen) > MaxContentSize)
+					break;
+
+				totalLen += packetLen;
+				packet->Id = m_currentValue + 1;
+
+				m_currentValue = (m_currentValue + 1) % MaxTelemetryCount;
+			} while (m_tmpValue != m_currentValue && (totalLen + 5) <= MaxContentSize);
+
+			m_tx.Length += totalLen;
+		}
+
+		UpdateBuffer();
+
+		//Bang them out
+		m_timer.Enable();
+		eventFlag.wait();
+
+	} while (m_run);
+
 	Reset();
 }
+
 void TelemetryJeti::Reset()
 {
 	m_timer.DisableIRQ(TIM_DIER_UIE);
@@ -62,32 +225,95 @@ void TelemetryJeti::Reset()
 	HAL::InterruptRegistry.Disable(m_timer.NVIC_IRQn);
 }
 
+void TelemetryJeti::UpdateBuffer()
+{
+	memset(m_tx.SendBuffer, 0x00, MaxBufferSize);
+
+	uint8_t crc = 0;
+	uint8_t i = 0;
+	uint8_t j = 0;
+
+	for (; i < (m_tx.Length + 2); i++)
+	{
+		m_tx.SendBuffer[i] = m_tx.Data[i];
+
+		if (i >= 1)
+			m_tx.SendBuffer[i] |= 0x100;
+
+		if (i >= 2)
+		{
+			crc ^= m_tx.Data[i];
+			for (j = 0; j < 8; j++)
+			{
+				crc = (crc & 0x80) ? CRCPoly ^ (crc << 1) : (crc << 1);
+			}
+		}
+	}
+
+	m_tx.SendBuffer[i++] = crc | 0x100;
+
+	//SimpleText part
+	m_tx.SendBuffer[i++] = 0xFE;
+
+	for (j = 0; j < MaxSimpleTextSize - 2; j++)
+		m_tx.SendBuffer[i++] = 0x00 | 0x100;
+
+	m_tx.SendBuffer[i++] = 0xFF;
+	m_tx.BufferLength = i;
+
+	m_tx.BufferPos = 0;
+	m_bitPos = 0;
+	m_parity = 0;
+}
+
 void TelemetryJeti::ISR()
 {
 	if (m_timer.GetInterruptSource(TIM_SR_UIF))
 	{
 		m_timer.ClearFlag(TIM_SR_UIF);
 
-		if (m_counter == 0) //Start Bit
-			m_pin.Off();
-		else if (m_counter < 10) //Data Bits
+		if (m_bitPos == 0) //Start Bit
 		{
-			m_counter % 2 == 0 ? m_pin.On() : m_pin.Off();
-		}
-		else if (m_counter < 11) //Parity Bit
 			m_pin.Off();
-		else if (m_counter < 13) //Stop Bits
+			m_tx.CurrentData = m_tx.SendBuffer[m_tx.BufferPos];
+		}
+		else if (m_bitPos <= 9) //Data Bits
+		{
+			m_parity ^= (m_tx.CurrentData & 0x01);
+
+			if ((m_tx.CurrentData & 0x01) == 0x01)
+				m_pin.On();
+			else
+				m_pin.Off();
+
+			m_tx.CurrentData >>= 1;
+		}
+		else if (m_bitPos <= 10) //Parity Bit
+			(m_parity ^ 0x01) == 0 ? m_pin.Off() : m_pin.On();
+		else if (m_bitPos <= 12) //Stop Bits
 			m_pin.On();
 
-		if (m_counter == 12)
-			m_timer.Disable();
+		if (m_bitPos == 12)
+		{
+			m_bitPos = 0;
+			m_parity = 0;
 
-		m_counter++;
+			m_tx.BufferPos++;
+			if (m_tx.BufferLength == m_tx.BufferPos)
+			{
+				m_timer.Disable();
+				eventFlag.signal_isr();
+			}
+		}
+		else
+			m_bitPos++;
 	}
 }
+
 void TelemetryJeti::DeInit()
 {
 	m_run = 0;
 	eventFlag.signal();
 }
+
 } /* namespace App */
